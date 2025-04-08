36c78a48cb526a579ebf13acfa871120974cfd93
tayaa710 <154112253+tayaa710@users.noreply.github.com>
2025-04-08 17:39:50 +0100

    Update FilterInOut.jsx

--- client/src/HomeScreen/filterBar/FilterInOut/FilterInOut.jsx
+++ client/src/HomeScreen/filterBar/FilterInOut/FilterInOut.jsx
@@ -1,5 +1,5 @@
 import CheckboxSelector from "./checkboxSelector/CheckboxSelector";
-import "./filterinout.css";
+import "./filterInOut.css";
 import { useState } from "react";
 
 const FilterInOut = ({ checkboxes, setCheckboxes, nameKey, title }) => {
